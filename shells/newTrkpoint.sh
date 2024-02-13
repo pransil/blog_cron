fd75ca1d63b14359d68b3d014182611336d385ec
Pat Ransil <pat.ransil@gmail.com>
2024-02-12 17:53:34 -0800

    Use $FPATH vars in shell

--- shells/newTrkpoint.sh
+++ shells/newTrkpoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+FPATH=/home/user/dev/blog/gpx_repo/blog_cron
 # This part only needed to fake the new trackpoint
 # updating latlon.txt (+0.1, -0.2 and adding the date/time)
-python /home/user/dev/blog/gpx_repo/python/calculateNewTrackpoint.py
+python $FPATH/python/calculateNewTrackpoint.py
 # Comvert latlon.txt to tracknewTrackpoint.gpx
-python https://github.com/pransil/pransil.github.io/python/newTrackpoint.py
+python $FPATH/python/newTrackpoint.py
 # newTrkpoint.sh - add a new trackpoint to data.gpx
-cat https://github.com/pransil/pransil.github.io/files/newTrackpoint.gpx >> https://github.com/pransil/pransil.github.io/files/trackpoints.gpx
-cat https://github.com/pransil/pransil.github.io/files/trackpoints.gpx > https://github.com/pransil/pransil.github.io/files/data.gpx
-cat https://github.com/pransil/pransil.github.io/files/trackEndLines.gpx >> https://github.com/pransil/pransil.github.io/files/data.gpx
-cat https://github.com/pransil/pransil.github.io/files/waypoints.gpx >> https://github.com/pransil/pransil.github.io/files/data.gpx
-cat https://github.com/pransil/pransil.github.io/files/gpxEndLine.gpx >> https://github.com/pransil/pransil.github.io/files/data.gpx
-git add https://github.com/pransil/pransil.github.io/files/data.gpx
+cat $FPATH/files/newTrackpoint.gpx >> $FPATH/files/trackpoints.gpx
+cat $FPATH/files/trackpoints.gpx > $FPATH/files/data.gpx
+cat $FPATH/files/trackEndLines.gpx >> $FPATH/files/data.gpx
+cat $FPATH/files/waypoints.gpx >> $FPATH/files/data.gpx
+cat $FPATH/files/gpxEndLine.gpx >> $FPATH/files/data.gpx
+cd ../../pransil.github.io
+git add https://github.com/pransil/pransil.github.i/files/data.gpx
 git commit -m "Added new trackpoint"
 git push --all
